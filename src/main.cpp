c29773547e4b45dee66e0e842901fd06c5fcfbf8
cj1917 <46627645+cj1917@users.noreply.github.com>
2020-02-02 17:36:58 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+/*
+Software for group 19 ME3 DMT project
+This project is to develop a low-cost, 3D printable MMG prosthetic hand,
+ATLAS
+*/
+
+
+/******************************************************************************/
+//include statements and boilerplate code
+
+#include <Arduino.h>
+#include "BandPassFilter.h"
+
+/******************************************************************************/
+// Define Global variables here
+
+
+/******************************************************************************/
+// interrupt service routines
+
+
+/******************************************************************************/
+// setup function, runs once when device is booted
+
+void setup() {
+  
+
+}
+
+/******************************************************************************/
+// main loop of the program
+
+void loop() {
+  
+
+}
+
+/******************************************************************************/
